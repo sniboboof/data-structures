1e6567d8906aa68e65c78f38aa531c503e0a0427
Jack Markley <jack.markley@gmail.com>
2014-02-26 18:55:46 +0100

    beginning hash

---
+++ hashtests.py
@@ -0,0 +1,16 @@
+import myhash
+import unittest
+
+class testHash(unittest.TestCase()):
+    """docstring for testHash"""
+    def setUp(self):
+        self.samplehash = None
+    
+    def testHash(self):
+        pass
+    
+    def testSet(self):
+        pass
+    
+    def testGet(self):
+        pass
---
+++ myhash.py
@@ -0,0 +1,4 @@
+class ezHash(object):
+    """docstring for ezHash"""
+    def __init__(self, max_size):
+        pass
