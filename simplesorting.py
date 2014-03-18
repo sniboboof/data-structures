2f0e03aab8c002c3694014467ec7a657b4de3dca
Jack Markley <jack.markley@gmail.com>
2014-03-18 19:42:37 +0100

    wrote the proper sort this time

--- simplesorting.py
+++ simplesorting.py
@@ -6,3 +6,13 @@ def selectionsort(mylist):
                 ansindex=j
         mylist[i], mylist[ansindex] = mylist[ansindex], mylist[i]
     return mylist
+
+def insertionsort(mylist):
+    for i in xrange(1, len(mylist)):
+        j=i-1
+        val = mylist[i]
+        while mylist[j] > val and j>=0:
+            mylist[j+1]=mylist[j]
+            mylist[j]=val
+            j-=1
+    return mylist
--- sorttests.py
+++ sorttests.py
@@ -3,7 +3,7 @@ import unittest
 import random
 import time
 
-class InsertCase(unittest.TestCase):
+class sortCase(unittest.TestCase):
     def testSelectionSort(self):
         answer = simplesorting.selectionsort([])
         assert answer == []
@@ -22,11 +22,37 @@ class InsertCase(unittest.TestCase):
         answer = simplesorting.selectionsort(sortinput)
         assert answer == range(100)
 
+    def testInsertionSort(self):
+        answer = simplesorting.insertionsort([])
+        assert answer == []
+        answer = simplesorting.insertionsort([0])
+        assert answer == [0]
+        answer = simplesorting.insertionsort([0, 1])
+        assert answer == [0, 1]
+        answer = simplesorting.insertionsort([1, 0])
+        assert answer == [0, 1]
+        answer = simplesorting.insertionsort([-1, 1, 0])
+        assert answer == [-1, 0, 1]
+        answer = simplesorting.insertionsort([5, 4, 2, 4, 7, -25, None, 12])
+        assert answer == [None, -25, 2, 4, 4, 5, 7, 12]
+        sortinput = range(100)
+        random.shuffle(sortinput)
+        answer = simplesorting.insertionsort(sortinput)
+        assert answer == range(100)
+
 def timeselection(abignumber):
     myinput = range(abignumber)
     random.shuffle(myinput)
     start = time.time()
     simplesorting.selectionsort(myinput)
+    stop = time.time()
+    print "%d values in %s" % (abignumber, stop-start)
+
+def timeinsertion(abignumber):
+    myinput = range(abignumber)
+    random.shuffle(myinput)
+    start = time.time()
+    simplesorting.insertionsort(myinput)
     stop = time.time()
     print "%d values in %s" % (abignumber, stop-start)
 
@@ -37,5 +63,11 @@ if __name__ == "__main__":
     timeselection(4000)
     timeselection(5000)
     timeselection(6000)
+    timeinsertion(1000)
+    timeinsertion(2000)
+    timeinsertion(3000)
+    timeinsertion(4000)
+    timeinsertion(5000)
+    timeinsertion(6000)
 
     unittest.main()
